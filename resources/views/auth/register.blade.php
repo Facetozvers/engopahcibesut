1ddcab3a1d32f2589a14312dd450fd7812d72e4c
Facetozvers <whatthefapmc@gmail.com>
2021-11-04 04:50:48 +0700

    jquery dependencies to https

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="/dependencies/bootstrap-5.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="http://code.jquery.com/jquery-2.2.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
     <link rel="stylesheet" href="/assets/css/main.css?v=1.22">
 
     <!-- Web Application Manifest -->
